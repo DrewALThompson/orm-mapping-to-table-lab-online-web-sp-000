823ff7c44931af8a4d97e231476de73e25b26361
Drew Thompson <drewallynthompson@gmail.com>
2020-10-04 00:40:31 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -10,13 +10,8 @@ class Student
   end
   
   def self.create_table
-    sql <<-SQL
-      CREATE TABLE IF NOT EXISTS students (
-      id INTEGER PRIMARY KEY,
-      name TEXT,
-      grade INTEGER
-      )
-    
+    sql = <<- SQL 
+      CREATE TABLE IF
     DB 
   end
 
